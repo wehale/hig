2f2401f7c6c8c61e21f6165147be46877695074f
Ezra Hale <ezra.hale@silabs.com>
2020-02-04 21:43:53 -0500

    fixed dup sentence issue #1

--- hig.js
+++ hig.js
@@ -3,6 +3,7 @@ var hig = fs.readFileSync('hig.txt').toString().split('\n');
 var i = 0;
 while (i < 5) { 
   var rand = Math.floor(Math.random() * Math.floor(hig.length));
-  console.log(hig[rand] + ". ");
+  var s = hig.splice(rand, 1); //remove the sentence into var s
+  console.log(s + ". "); //print out the sentence
   i++;
 }
