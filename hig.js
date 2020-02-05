6613e88694989effb3bb19217fca9b69a38cc05e
Ezra Hale <ezra.hale@silabs.com>
2020-02-04 22:05:24 -0500

    Now prints out a random number of sentences from 1 - 10

--- hig.js
+++ hig.js
@@ -1,7 +1,9 @@
+// Generates a series of sentences based on 
 var fs = require('fs');
-var hig = fs.readFileSync('hig.txt').toString().split('\n');
+var hig = fs.readFileSync('hig.txt').toString().trim().split('\n');
 var i = 0;
-while (i < 5) { 
+var count = Math.random() * 10; //get random num from 1 - 10
+while (i < count) { 
   var rand = Math.floor(Math.random() * Math.floor(hig.length));
   var s = hig.splice(rand, 1); //remove the sentence into var s
   console.log(s + ". "); //print out the sentence
