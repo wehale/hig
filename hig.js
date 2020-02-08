4513ab26520d7cf3e9967b50e7bbe8e2ecf6b6db
Ezra Hale <ezra.hale@silabs.com>
2020-02-07 23:26:30 -0500

    added teams and more players

--- hig.js
+++ hig.js
@@ -2,62 +2,59 @@
 var fs = require('fs');
 let rawJson = fs.readFileSync('hig.json');
 let hig = JSON.parse(rawJson);
-let player = hig.players;
+
+let team = hig.teams;
 let statement = hig.statements;
 let filler = hig.filler;
 let action = hig.actions;
 let closing = hig.closing;
 let start = hig.starts;
 let question = hig.questions;
+var rand;
 
-var i = 0;
-var count = Math.random() * 10; //get random num from 1 - 10
+//Team
+rand = Math.floor(Math.random() * Math.floor(team.length));
+var t = team[rand]
 
 //Player
-var rand = Math.floor(Math.random() * Math.floor(player.length));
-var p = player[rand]
+rand = Math.floor(Math.random() * Math.floor(t.players.length));
+var p = t.players[rand]
 
-//Intro
+//Question
 rand = Math.floor(Math.random() * Math.floor(question.length));
 var q = question[rand];
-console.log("(Announcer): Thanks John, I'm here with " + p.name);
+console.log("(Announcer): Thanks John, I'm here with " + p.name + " of the " + t.name);
 console.log("(Announcer): So " + p.nickname + " " + q +"?");
 
 //Start
-var rand = Math.floor(Math.random() * Math.floor(start.length));
-var s = start.splice(rand, 1); //remove the sentence into var s
-console.log(p.nickname + ": " + s + ". "); //print out the sentence
+printLine(p, start, ",");
+
+//Interview of random length
+var i = 0;
+var count = Math.random() * 10; //get random num from 1 - 10
 while (i < count) { 
-
-
 	//Possible Filler
 	if ((i > 0) && (Math.random() < 0.5)) {
-		//Filler
-		rand = Math.floor(Math.random() * Math.floor(filler.length));
-		s = filler.splice(rand, 1); //remove the sentence into var s
-		console.log(p.nickname + ": " + s + ","); //print out the sentence
+		printLine(p, filler, ",");
 	}
-
 	//Possible Action
 	if (Math.random() < 0.5) {
-		//Action
-		rand = Math.floor(Math.random() * Math.floor(action.length));
-		s = action.splice(rand, 1); //remove the sentence into var s
-		console.log(p.nickname + ": " + s + ". "); //print out the sentence
+		printLine(p, action, " ");
 	}
-
 	//Statement
-	rand = Math.floor(Math.random() * Math.floor(statement.length));
-	s = statement.splice(rand, 1); //remove the sentence into var s
-	console.log(p.nickname + ": " + s + ". "); //print out the sentence
-	
+	printLine(p, statement, ".");
 	i++;
 }
 
-//Intro
+//Announcer closing
 console.log("(Announcer): Ok, that's great to hear, thanks " + p.nickname + ".");
 
-//Closing
-rand = Math.floor(Math.random() * Math.floor(closing.length));
-s = closing.splice(rand, 1); //remove the sentence into var s
-console.log(p.nickname + ": " + s + ". "); //print out the sentence
+//Player closing
+printLine(p, closing, ".");
+
+
+function printLine(p, a, punct) {
+	var rand = Math.floor(Math.random() * Math.floor(a.length));
+    var s = a.splice(rand, 1); //remove the sentence into var s
+	console.log(p.nickname + ": " + s + punct); //print out the sentence
+}
