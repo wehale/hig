bfe0ff8be6bb7151451211878b58094d361a6b84
Ezra Hale <ezra.hale@silabs.com>
2020-03-02 08:59:43 -0500

    Updated nicknames, removed test.js

--- test.js
+++
@@ -1,8 +0,0 @@
- 
-// or, override the platform
-const Say = require('say').Say
-const say = new Say('darwin' || 'win32' || 'linux')
- 
-// Use default system voice and speed
-say.speak('Hello!')
- 
