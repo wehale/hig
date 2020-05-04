7cba0a026e2a353da6efdce75ca560c5d8ce8a68
Paul Regan <paregan@silabs.com>
2020-05-04 18:10:12 -0400

    deleting debug code

--- hig.js
+++ hig.js
@@ -101,7 +101,6 @@ function speak(phrases) {
 		}
 	}
 	var ph = p.phrase.toString().startsWith('(') ? "..." : p.phrase;
-	say.getInstalledVoices((err, voices) => console.log(voices))
 	let p1 = new Promise(
         // The executor function is called with the ability to resolve or
         // reject the promise
